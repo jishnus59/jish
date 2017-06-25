50109f04c88fcd6ab318b593b25673097840291a
jishnus59 <jishnus59@gmail.com>
2017-06-25 15:29:38 +0530

    Add files via upload

---
+++ powerFunction/R/powerFunction.R
@@ -0,0 +1,36 @@
+
+#' Power Function
+#' 
+#' Takes numeric in puts x & y
+#' @author  Jishnu
+#' @param x numeric variable
+#' @param y numeric variable
+#' @return x^y 
+#' @example example.R
+#' @export
+Power<- function(x,y){
+  return(x^y)
+}
+#' Squire Function
+#' 
+#' Takes numeric in puts x 
+#' @author  Jishnu
+#' @param x numeric variable
+#' @return x^2 
+#' @example power_example.R
+#' @export
+Squire<-function(x){
+  return(x^2)
+}
+
+#' Cubic Function
+#' 
+#' Takes numeric in puts x 
+#' @author  Jishnu
+#' @param x numeric variable
+#' @return x^3 
+#' @example Cube.R
+#' @export
+Cube<-function(x){
+  return(x^3)
+}
